13bb93314e10551f82be282ae3c5f53b88856c7f
breathfish <470885118@qq.com>
2017-08-21 15:31:50 +0800

    提交test.js

---
+++ EasyFun/src/common/test.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+class TestComponent extends Component {
+	constructor(prop){
+		super(prop);
+	}
+	render(){
+		return (
+			<div>this is test component</div>
+		)
+	}
+}
+export default TestComponent;
